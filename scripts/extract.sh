5fe8f1fbcd353cffc450c96cc46717736bbeafd2
Jaromír Pufler <jaromir.pufler@gmail.com>
2016-08-30 10:49:41 +0200

    extracting exit on error

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /opt/trinitycore/data
-time ../bin/mapextractor
-time ../bin/vmap4extractor
+time ../bin/mapextractor || exit 1
+time ../bin/vmap4extractor || exit 1
 mkdir vmaps
-time ../bin/vmap4assembler Buildings vmaps
+time ../bin/vmap4assembler Buildings vmaps || exit 1
 mkdir mmaps
 time ../bin/mmaps_generator --threads `nproc`
 
